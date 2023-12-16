27a1c8bde1bc84892358d9f09736e9dc025bedae
romer8 <gio.busrom@gmail.com>
2023-12-16 11:12:09 -0700

    added stations from services

--- tethysapp/hydroserver_client/controllers.py
+++ tethysapp/hydroserver_client/controllers.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from tethys_sdk.routing import controller
+import requests
+
+HYDROSERVER_ENDPOINT = 'https://hydroserver.geoglows.org'
 
 @controller
 def home(request):
     """
     Controller for the app home page.
     """
+    headers = {'accept': 'application/json'}
 
-
+    things_list = get_things(headers)
+    print(things_list)
     context = {
-
+        'things_list': things_list
     }
 
     return render(request, 'hydroserver_client/home.html', context)
+
+
+
+def get_things(headers):
+    things = []
+
+    try:
+        url_things = f'{HYDROSERVER_ENDPOINT}/api/data/things'
+        response = requests.get(url_things, headers=headers)
+        if response.status_code == 200:
+            things = response.json()
+    except Exception as e:
+        print(e)
+    return things
--- tethysapp/hydroserver_client/public/js/main.js
+++ tethysapp/hydroserver_client/public/js/main.js
@@ -12,7 +12,45 @@
           zoom: 2,
         }),
       });
+        getThings(map);
+
     };
+
+    const getThings = (map) => {
+
+        const thingsListSerializedData = document.getElementById('things-list').textContent;
+        const thingsListParsedData = JSON.parse(thingsListSerializedData);
+
+        // Assuming 'map' is your OpenLayers map
+        const vectorLayer = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+            style: new ol.style.Style({
+                image: new ol.style.Circle({
+                  radius: 5,
+                  fill: new ol.style.Fill({
+                    color: '#8e44ad',
+                  }),
+                  stroke: new ol.style.Stroke({
+                    color: 'white',
+                    width: 1,
+                  }),
+                }),
+            })
+        });
+        
+        map.addLayer(vectorLayer);
+        
+        thingsListParsedData.forEach(item => {
+            const marker = new ol.Feature({
+            geometry: new ol.geom.Point(
+                ol.proj.fromLonLat([item.longitude, item.latitude])
+            ),
+            });
+        
+            vectorLayer.getSource().addFeature(marker);
+        })
+      };
+  
 
     initializeMap();
   })();
